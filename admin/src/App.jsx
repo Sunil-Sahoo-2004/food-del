aef0eb340833fde2822c062c6ca477824cc699be
Sunil Sahoo <sunilsahoo1525@gmail.com>
2024-12-24 02:16:03 +0530

    Second1

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-  const url = "https://food-del-backend-l3nu.onrender.com";
+  // const url = "https://food-del-backend-l3nu.onrender.com";
 
   return (
     <div>
